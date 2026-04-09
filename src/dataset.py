614bbbf09ffbd83f84d8c33c01f2151e9d8091f2
Aarya2304 <aarya.yadav@mitwpu.edu.in>
2026-04-09 21:45:53 +0530

    Added dataset loading pipeline using Pytorch ImageFolder

---
+++ src/dataset.py
@@ -0,0 +1,16 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
+
+data_path = "data/real_and_fake_face"
+
+dataset = datasets.ImageFolder(root=data_path, transform=transform)
+
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+print("Classes:", dataset.classes)
